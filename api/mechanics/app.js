e4c7ce39ecfe7039f39853520d18ec0e2cd01330
Nawin P <nawinc.work@gmail.com>
2019-05-02 21:54:16 +0700

    Update app.js

--- api/mechanics/app.js
+++ api/mechanics/app.js
@@ -8,9 +8,12 @@ app.use(urlencodedParser = bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 app.use(cors())
 app.options('*', cors())
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Headers", "*")
+})
 
 const mechanicsController = require('./controller/mechanicsController')
 
-app.use('/api/mechanics',mechanicsController)
+app.use('/api/mechanics', mechanicsController)
 
 module.exports = app
