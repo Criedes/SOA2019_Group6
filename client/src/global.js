eac36f9463753ad8c2dca5e1161e27e3a2656062
Nawin P <nawinc.work@gmail.com>
2019-05-05 14:13:09 +0700

    Update global.js

--- client/src/global.js
+++ client/src/global.js
@@ -7,5 +7,6 @@ module.exports.LOGIN_PATH_MECHANIC = 'http://139.162.25.245/api/auth/mechanic'
 module.exports.AUTH_PATH = 'http://139.162.25.245/api/auth/authentication'
 
 module.exports.LOAD_MECHANIC_ALL = 'http://139.162.25.245/api/mechanics/find'
-module.exports.GET_PROFILE_MECHANIC = 'http://localhost:3004/api/mechanics/find' //+ _id
-module.exports.SEARCH_MECHANIC_BY_NAME = 'http://localhost:3004/api/mechanics/search'
+module.exports.GET_PROFILE_MECHANIC = 'http://139.162.25.245:3004/api/mechanics/find' //+ _id
+module.exports.SEARCH_MECHANIC_BY_NAME = 'http://139.162.25.245:3004/api/mechanics/search'
+
