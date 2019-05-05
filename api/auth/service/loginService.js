d90f17af23a02e5e18f3681b3a22bd73a5c5ece6
EvilMonarch <59070016@kmitl.ac.th>
2019-05-05 15:25:31 -0700

    Update loginService
    -

--- api/auth/service/loginService.js
+++ api/auth/service/loginService.js
@@ -6,82 +6,65 @@ const mechanicSchema = require('../model/mechanicSchema')
 const jwt = require('jsonwebtoken')
 const config = require('config')
 
-
 exports.authenUser = async (req, res) => {
-    
-    const {error} = validateUser(req.body)
-
-    if(error) {
+    const { error } = validateUser(req.body)
+    if (error) {
         console.log(req.body.username)
         console.log(req.body.password)
         console.log(error)
         return res.status(400).send(error.details[0].message)
-
     }
-    
+
     const username = req.body.username;
     const password = req.body.password;
-    const user = await customerSchema.findOne({username:req.body.username})
-    if(!user) return res.status(400).json({msg:"invalid email or password"})
-
+    const user = await customerSchema.findOne({ username: req.body.username })
+    if (!user) return res.status(400).json({ msg: "invalid email or password" })
     const validPassword = await bcrypt.compare(req.body.password, user.password)
 
-    if(!validPassword) return res.status(400).json({msg:"Password incorrect"})
-
+    if (!validPassword) return res.status(400).json({ msg: "Password incorrect" })
     const payload = {
         _id: user._id,
         username: user.username,
         role: 'customer'
     }
-    
-    jwt.sign(payload, config.get('jwtPrivateKey'), {expiresIn:360000} , (err, token)=>{
+
+    jwt.sign(payload, config.get('jwtPrivateKey'), { expiresIn: 360000 }, (err, token) => {
         // console.log(token)
-        res.header('Authorization', token).status(201).json({token})
+        res.header('Authorization', token).status(201).json({ token })
     })
-    
-    
 }
 
-
 exports.authenMechanic = async (req, res) => {
-    
-    const {error} = validateUser(req.body)
-
-    if(error) {
+    const { error } = validateUser(req.body)
+    if (error) {
         console.log(req.body.username)
         console.log(req.body.password)
         console.log(error)
         return res.status(400).send(error.details[0].message)
-
     }
-    
+
     const username = req.body.username;
     const password = req.body.password;
-    const user = await mechanicSchema.findOne({username:req.body.username})
-    if(!user) return res.status(400).json({msg:"invalid email or password"})
-
+    const user = await mechanicSchema.findOne({ username: req.body.username })
+    if (!user) return res.status(400).json({ msg: "invalid email or password" })
     const validPassword = await bcrypt.compare(req.body.password, user.password)
 
-    if(!validPassword) return res.status(400).json({msg:"Password incorrect"})
-
+    if (!validPassword) return res.status(400).json({ msg: "Password incorrect" })
     const payload = {
         _id: user._id,
         username: user.username,
         role: 'mechanic'
     }
-    
-    jwt.sign(payload, config.get('jwtPrivateKey'), {expiresIn:360000} , (err, token)=>{
+
+    jwt.sign(payload, config.get('jwtPrivateKey'), { expiresIn: 360000 }, (err, token) => {
         // console.log(token)
-        res.header('Authorization', token).status(201).json({token})
+        res.header('Authorization', token).status(201).json({ token })
     })
-    
-    
 }
-function validateUser(req){
+function validateUser(req) {
     const schemaTestObj = {
         username: Joi.string().min(5).max(255).required(),
-        password : Joi.string().min(5).max(255).required()
+        password: Joi.string().min(5).max(255).required()
     }
-
     return Joi.validate(req, schemaTestObj)
 }
