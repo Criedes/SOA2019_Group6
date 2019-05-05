64cd08d87a3e4e5fd57426ff9a5144f3a9fd11c0
PoundJira <59070026@it.kmitl.ac.th>
2019-05-05 19:14:45 +0700

    edit path of api

--- client/src/global.js
+++ client/src/global.js
@@ -1,9 +1,10 @@
-module.exports.REGISTER_PATH_CUSTOMER = 'http://139.162.25.245/api/customers/register';
-module.exports.LOGIN_PATH_CUSTOMER = 'http://139.162.25.245/api/auth/customer'
+module.exports.REGISTER_PATH_CUSTOMER = 'https://payangonline.me/api/customers/register';
+module.exports.LOGIN_PATH_CUSTOMER = 'https://payangonline.me/api/auth/customer'
 module.exports.LOGIN_PATH_MECHANIC = 'http://localhost:3004/api/auth/mechanic'
 
 module.exports.AUTH_PATH = 'http://localhost:3004/api/auth/authentication'
 
-module.exports.LOAD_MECHANIC_ALL = 'http://139.162.25.245/api/mechanics/find'
-module.exports.GET_PROFILE_MECHANIC = 'http://139.162.25.245/api/mechanics/find' //+ _id
-module.exports.SEARCH_MECHANIC_BY_NAME = 'http://139.162.25.245/api/mechanics/search'
+module.exports.LOAD_MECHANIC_ALL = 'https://payangonline.me/api/mechanics/find'
+module.exports.GET_PROFILE_MECHANIC = 'https://payangonline.me/api/mechanics/find' //+ _id
+module.exports.SEARCH_MECHANIC_BY_NAME = 'https://payangonline.me/api/mechanics/search'
+
