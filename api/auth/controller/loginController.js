a37fc6e29736ef8e26cdb7905aaf80d9e7aa8786
PoundJira <59070026@it.kmitl.ac.th>
2019-05-07 02:18:51 +0700

    change metrics prometheus request

--- api/auth/controller/loginController.js
+++ api/auth/controller/loginController.js
@@ -23,14 +23,26 @@ router.get('/authentication', auth, async (req, res) => {
     }
 })
 
+const counter = new Prometheus.Counter({
+    name: 'request_total',
+    help: 'metric_help'
+  });
+  
+
+router.post('/customer', ()=>{
+    loginService.authenUser
+    counter.inc();
+})
+router.post('/mechanic', ()=>{
+    loginService.authenMechanic
+    counter.inc();
+})
+
 router.get('/metrics', (request, response) => {
     response.set('Content-Type', Prometheus.register.contentType)
     console.log(Prometheus.register)
     response.send(Prometheus.register.metrics())
 })
-
-router.post('/customer', loginService.authenUser)
-router.post('/mechanic', loginService.authenMechanic)
 
 //test check priority.
 router.get('/tester2', auth, (req, res) => {
