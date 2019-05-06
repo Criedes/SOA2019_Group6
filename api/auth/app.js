7309d43d74c170f4686102552d3486191b92bced
nailtail <kalostn2099@hotmail.com>
2019-05-07 00:48:32 +0700

    add prometheus to auth

--- api/auth/app.js
+++ api/auth/app.js
@@ -4,10 +4,25 @@ const app = express()
 const cors = require('cors')
 const port = 3000
 const Eureka = require('eureka-js-client').Eureka;
+const Prometheus = require('prom-client')
+
 var jsonParser = bodyParser.json()
 app.use(urlencodedParser = bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 app.use(cors());
+
+const metricsInterval = Prometheus.collectDefaultMetrics()
+const checkoutsTotal = new Prometheus.Counter({
+  name: 'checkouts_total',
+  help: 'Total number of checkouts',
+  labelNames: ['payment_method']
+})
+const httpRequestDurationMicroseconds = new Prometheus.Histogram({
+  name: 'http_request_duration_ms',
+  help: 'Duration of HTTP requests in ms',
+  labelNames: ['method', 'route', 'code'],
+  buckets: [0.10, 5, 15, 50, 100, 200, 300, 400, 500]  // buckets for response time from 0.1ms to 500ms
+})
 
 const client = new Eureka({
     // application instance information
@@ -37,6 +52,28 @@ const client = new Eureka({
         servicePath: '/eureka/apps/',
     },
 });
+
+// Prometheus
+app.use((req, res, next) => {
+    res.locals.startEpoch = Date.now()
+    next()
+  })
+
+app.get('/api/auth/metrics', (req, res) => {
+    res.set('Content-Type', Prometheus.register.contentType)
+    res.end(Prometheus.register.metrics())
+  })
+
+app.use((req, res, next) => {
+    const responseTimeInMs = Date.now() - res.locals.startEpoch
+  
+    httpRequestDurationMicroseconds
+      .labels(req.method, req.route.path, res.statusCode)
+      .observe(responseTimeInMs)
+  
+    next()
+  })
+// End Prometheus
 
 const loginController = require('./controller/loginController');
 client.logger.level('debug');
