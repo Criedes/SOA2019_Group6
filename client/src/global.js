f67791221d225159adb07b0a930508c362f6eb1c
Nawin P <nawinc.work@gmail.com>
2019-05-07 05:37:53 +0700

    Update global.js

--- client/src/global.js
+++ client/src/global.js
@@ -8,5 +8,5 @@ module.exports.LOAD_MECHANIC_ALL = 'https://payangonline.me/api/mechanics/find'
 module.exports.GET_PROFILE_MECHANIC = 'https://payangonline.me/api/mechanics/find' //+ _id
 module.exports.SEARCH_MECHANIC_BY_NAME = 'https://payangonline.me/api/mechanics/search'
 
-module.exports.OPEN_SOCKET = 'http://localhost:3005'
+module.exports.OPEN_SOCKET = 'https://payangonline.me'
 
