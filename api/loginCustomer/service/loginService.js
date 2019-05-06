57776577e8fcd81d1073449941a2eecd0d750f0f
EvilMonarch <59070016@kmitl.ac.th>
2019-05-06 12:42:11 -0700

    update loginService
    -

--- api/loginCustomer/service/loginService.js
+++ api/loginCustomer/service/loginService.js
@@ -6,28 +6,28 @@ const customerSchema = require('../model/customerSchema')
 
 
 exports.authenUser = async (req, res) => {
-    
-    const {error} = validateUser(req.body)
 
-    if(error) return res.status(400).send(error.details[0].message)
+    const { error } = validateUser(req.body)
+
+    if (error) return res.status(400).send(error.details[0].message)
     const username = req.body.username;
     const password = req.body.password;
-    const user = await customerSchema.findOne({username:req.body.username})
-    if(!user) return res.status(400).send("Invalid email or password")
+    const user = await customerSchema.findOne({ username: req.body.username })
+    if (!user) return res.status(400).send("Invalid email or password")
 
     const validPassword = await bcrypt.compare(req.body.password, user.password)
 
-    if(!validPassword) return res.status(400).send("Invalid email or password")
+    if (!validPassword) return res.status(400).send("Invalid email or password")
 
     res.json(true)
-    
+
 }
 
 
-function validateUser(req){
+function validateUser(req) {
     const schemaTestObj = {
         username: Joi.string().min(5).max(255).required(),
-        password : Joi.string().min(5).max(255).required()
+        password: Joi.string().min(5).max(255).required()
     }
 
     return Joi.validate(req, schemaTestObj)
