78843197f42ed6e5b3caf38c18896f3936438f92
nailtail <kalostn2099@hotmail.com>
2019-05-07 10:02:41 +0700

    remove comment eureka

--- api/mechanics/app.js
+++ api/mechanics/app.js
@@ -10,39 +10,39 @@ app.use(bodyParser.json());
 app.use(cors())
 
 const mechanicsController = require('./controller/mechanicsController')
-// const client = new Eureka({
-//     // application instance information
-//     instance: {
-//         app: 'mechanic',
-//         hostName: process.env.EUREKA_CLIENT_HOST || 'localhost',
-//         ipAddr: '127.0.0.1',
-//         statusPageUrl: (process.env.EUREKA_CLIENT_URL || 'http://localhost:') + port,
-//         vipAddress: 'mechanic',
-//         port: {
-//             $: port,
-//             '@enabled': 'true',
-//         },
-//         dataCenterInfo: {
-//             '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
-//             name: 'MyOwn',
-//         },
-//         registerWithEureka: true,
-//         fetchRegistry: true,
-//         leaseRenewalIntervalInSeconds: 1,
-//         leaseExpirationDurationInSeconds: 2
-//     },
-//     eureka: {
-//         // Eureka server
-//         host: process.env.EUREKA_SERVER_HOST || 'localhost',
-//         port: process.env.EUREKA_SERVER_PORT || 8761,
-//         servicePath: '/eureka/apps/',
-//     },
-// });
+const client = new Eureka({
+    // application instance information
+    instance: {
+        app: 'mechanic',
+        hostName: process.env.EUREKA_CLIENT_HOST || 'localhost',
+        ipAddr: '127.0.0.1',
+        statusPageUrl: (process.env.EUREKA_CLIENT_URL || 'http://localhost:') + port,
+        vipAddress: 'mechanic',
+        port: {
+            $: port,
+            '@enabled': 'true',
+        },
+        dataCenterInfo: {
+            '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+            name: 'MyOwn',
+        },
+        registerWithEureka: true,
+        fetchRegistry: true,
+        leaseRenewalIntervalInSeconds: 1,
+        leaseExpirationDurationInSeconds: 2
+    },
+    eureka: {
+        // Eureka server
+        host: process.env.EUREKA_SERVER_HOST || 'localhost',
+        port: process.env.EUREKA_SERVER_PORT || 8761,
+        servicePath: '/eureka/apps/',
+    },
+});
 
-// client.logger.level('debug');
-// client.start((error) => {
-//     console.log(error || 'Eureka client started');
-//     app.use('/api/mechanics', mechanicsController)
-// });
-app.use('/api/mechanics', mechanicsController)
+client.logger.level('debug');
+client.start((error) => {
+    console.log(error || 'Eureka client started');
+    app.use('/api/mechanics', mechanicsController)
+});
+// app.use('/api/mechanics', mechanicsController)
 module.exports = app
