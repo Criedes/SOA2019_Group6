d66ea2c320fd4d2763af9e775025bd0fa42f4874
EvilMonarch <59070016@kmitl.ac.th>
2019-05-06 19:06:30 -0700

    Update mechanicsController.test
    -

--- api/mechanics/test/mechanic/controller/mechanicsController.test.js
+++ api/mechanics/test/mechanic/controller/mechanicsController.test.js
@@ -6,24 +6,20 @@ describe('/api/mechanics',()=>{
     beforeAll(async ()=>{
         console.log('change port to 3000 to test')
     })
-
     afterAll(()=>{
         server.close();
         console.log('test complete & server.close')
     })
-    
     describe('GET /find', ()=>{
         it('should return all mechanics', async ()=>{
             const res = await request(server).get('/api/mechanics/find')
             expect(res.status).toBe(200)
         })
     })
-
     describe('GET /find/:id', ()=>{
         it('should return mechanic by id', async ()=>{
             let test_Id = '5c9001f5cf9f9075a756e419'
             const res = await request(server).get('/api/mechanics/find/'+test_Id)
-
             expect(res.body[0]).toHaveProperty("username", "jameonline")
             expect(res.body[0]).toHaveProperty("coordinate", { lat: '29.001993213', lng: '18.09002313' })
             expect(res.body[0]).toHaveProperty("_id", '5c9001f5cf9f9075a756e419' )
@@ -34,7 +30,6 @@ describe('/api/mechanics',()=>{
             expect(res.status).toBe(200)
         })
     })
-
     describe('GET /find/garage/:garagename', ()=>{
         it('should return mechanic information by garage name', async ()=>{
             let test_garage_name = 'Chang James Shop'
@@ -49,21 +44,18 @@ describe('/api/mechanics',()=>{
             expect(res.status).toBe(200)
         })
     })
-
     describe('PUT /updatestatus/:id', ()=>{
         it('should update status of mechanic', async ()=>{
             const res = await request(server).put('/api/mechanics/updatestatus/5c9001f5cf9f9075a756e419')
             expect(res.status).toBe(200)
         })
     })
-
     describe('PUT /updatecounter/:id', ()=>{
         it('should update a number of customer s mechanic by id', async ()=>{
             const res = await request(server).put('/api/mechanics/updatecounter/5c9001f5cf9f9075a756e419')
             expect(res.status).toBe(200)
         })
     })
-
     describe('POST /register with correctness data', ()=>{
         const case1 = {
             "coordinate": {
@@ -94,7 +86,6 @@ describe('/api/mechanics',()=>{
             expect(res.status).toBe(201)
         })        
     })
-
     describe('POST /register with Incorrectness data', ()=>{
         const case2 = {
             "coordinate": {
